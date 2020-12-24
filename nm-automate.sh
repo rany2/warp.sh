514b8774cdc66565cc062a8bdf5a0bb23ae36a56
rany2 <rany2@riseup.net>
2020-12-24 16:39:47 +0200

    Add 1.0.0.1 and all IPv6 DNS for 1.1.1.1 to prevent IPv6 DNS leak

--- nm-automate.sh
+++ nm-automate.sh
@@ -7,3 +7,6 @@
 ./warp.sh -4
 nmcli connection delete warp
 nmcli connection import file warp.conf type wireguard
+nmcli connection modify warp ipv4.dns-priority -42
+nmcli connection modify warp ipv6.dns-priority -42
+nmcli connection modify warp connection.autoconnect yes
