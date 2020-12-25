746656dd286b293a78fbb8d1abf23aa762164b0e
rany2 <rany2@riseup.net>
2020-12-25 15:01:11 +0200

    Finally fix with proper client ciphers

--- warp.sh
+++ warp.sh
@@ -20,7 +20,9 @@ show_trace() {
 	curl "${curlopts[@]}" "https://1.1.1.1/cdn-cgi/trace"
 }
 
-curlopts=( --header 'User-Agent: okhttp/3.12.1' --header 'Content-Type: application/json' --silent --compressed --tlsv1.3 )
+curlopts=( --header 'User-Agent: okhttp/3.12.1' --header 'Content-Type: application/json' --silent --compressed '--tlsv1.0' --tls-max 1.3 )
+curlopts+=( --ciphers 'TLS_RSA_WITH_AES_128_GCM_SHA256:TLS_RSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256:TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256' )
+curlopts+=( '--tls13-ciphers' 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256' )
 wgoverride=0
 wgproto=0
 status=0
