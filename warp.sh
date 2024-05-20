rany2/warp.sh

commit 297daf745e467b39d4bbdc74f63801249e867230

Fix bug with refreshed config not showing token anymore
Token is only shown once and never sent again in response,
it should be taken from user's args instead.

Signed-off-by: rany <ranygh@riseup.net>

warp.sh
@@ -188,7 +188,9 @@ if [ -z "${account_license}" ] && [ -n "${teams_ephemeral_token}" ]; then
 elif [ -z "${account_license}" ]; then
 	account_license="Unknown"
 fi
-token=$(printf %s "${cf_creds}" | awk 'NR==4')
+if [ -z "${token}" ]; then
+	token=$(printf %s "${cf_creds}" | awk 'NR==4')
+fi
 
 # Write WARP Wireguard config and quit
 cat <<-EOF

commit 9abcd084e427fd5c37bf0dc652e6f7f97b164526

Initialize token variable
When fixing the bug with token not being set in the WG config when using
the newly added refresh config feature, I accidently introduces this bug.
Fix it by initalizing the variable.

Fixes: 297daf7 ("Fix bug with refreshed config not showing token anymore")
Fixes: #11
Signed-off-by: rany <ranygh@riseup.net>

warp.sh
@@ -26,6 +26,7 @@ curl_opts=
 refresh_token=
 show_regonly=0
 teams_ephemeral_token=
+token=
 
 # Helper function to send traffic to Cloudflare API without
 # tripping up their TLS fingerprinting mechanism and triggering